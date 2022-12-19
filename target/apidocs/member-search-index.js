a8b2d46a6e6570edb9717ddc6e9addf6b763a30c
ganzi <ganzi324@cloocus.com>
2022-12-19 14:36:11 +0900

    first commit

---
+++ target/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"org.springframework.cloud.stream.app.jdbc.sink.rabbit","c":"JdbcSinkRabbitApplication","l":"JdbcSinkRabbitApplication()","u":"%3Cinit%3E()"},{"p":"org.springframework.cloud.stream.app.jdbc.sink.rabbit","c":"JdbcSinkRabbitApplication","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ target/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.springframework.cloud.stream.app.jdbc.sink.rabbit","l":"JdbcSinkRabbitApplication"}];updateSearchResults();
